e1a9fc077b700a62b0fc5f69d22d54fede4ec8a6
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-05-26 23:26:27 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,7 +15,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if board[index].between(0, 8) && board[index] == " "
+  if board[index] >= 0 && board[index] <= 8 && position_taken?(board, index)
     return true
   else
     return false
