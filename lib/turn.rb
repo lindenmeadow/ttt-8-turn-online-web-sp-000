5f26babf7537342c91c21929bbb4989d343e209f
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-05-26 23:22:24 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,7 +15,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if board[index].between(0, 8)? && position_taken?
+  if board[index].between(0, 8) && (board[index] == " " || board[index] == "" || board[index] == nil)
     return true
   else
     return false
